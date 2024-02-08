57a251f352e1ae6c783cb235dc57f9000b1ff5ba
Piyush-onGIT <piyush.at.googl@gmail.com>
2024-02-08 15:26:22 +0530

    added rotate button

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ const Page = () => {
   const [data, setData] = useState<Data[]>([]);
   const containerRef = useRef<HTMLDivElement>(null);
   const [scrollInterval, setScrollInterval] = useState<any | null>(null);
+  const [deg, setDeg] = useState('90');
 
   useEffect(() => {
     const getData = async () => {
@@ -26,7 +27,7 @@ const Page = () => {
         if (JSON.stringify(newData) !== JSON.stringify(data)) {
           setData(newData);
         }
-        
+
         startAutoScroll();
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -60,24 +61,49 @@ const Page = () => {
     }
   };
 
+  const rotatePage = () => {
+    if (containerRef.current) {
+      containerRef.current.style.transform = `rotate(${deg}deg)`;
+      const container = containerRef.current;
+      if (deg === '90')
+        setDeg('0');
+      else
+        setDeg('90')
+    }
+  };
+
   return (
-    <div className="w-screen h-full p-2" onMouseEnter={stopAutoScroll} onMouseLeave={startAutoScroll}>
-      <img src={'/logo.png'} alt="logo" className="h-14" />
-      <div
-        className="w-full flex gap-4 p-2 overflow-hidden h-full"
-        ref={containerRef}
-        style={{ whiteSpace: "nowrap" }}
-      >
-        {data.map((item, index) => (
-          <div key={index} className="flex flex-col items-center min-w-[250px]">
-            <img
-              src={item.image}
-              className="h-96 object-cover rounded-2xl"
-            />
-          </div>
-        ))}
+    <>
+      <div className="w-screen h-full p-2" onMouseEnter={stopAutoScroll} onMouseLeave={startAutoScroll}>
+        <img src={'/logo.png'} alt="logo" className="h-14" />
+        <div
+          className="w-full flex gap-4 p-2 overflow-hidden h-full"
+          ref={containerRef}
+          style={{ whiteSpace: "nowrap" }}
+        >
+          {data.map((item, index) => (
+            <div key={index} className="flex flex-col items-center min-w-[250px]">
+              <img
+                src={item.image}
+                className="h-96 object-cover rounded-2xl"
+              />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+      <button
+        style={{
+          color: 'white',
+          position: 'fixed',
+          bottom: '20px',
+          right: '20px',
+          zIndex: '999',
+        }}
+        onClick={rotatePage}
+      >
+        Rotate
+      </button>
+    </>
   );
 };
 
